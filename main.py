978f7939161b51e0f5428d9a10aba429610a4817
Abigail Faron <afaron@AFaronMBA.local>
2021-08-03 15:29:02 -0500

    Added animal comments

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+# Dog goes "woof"
+# Cat goes "meow"
+# Bird goes "tweet"
+# And mouse goes "squeek"
+# Cow goes "moo"
+# Frog goes "croak"
+# And the elephant goes "toot"
+# Ducks say "quack"
+# And fish go "blub"
+# And the seal goes "ow ow ow"
+# But there's one sound
+# That no one knows
+# What does the fox say?
+# "Ring-ding-ding-ding-dingeringeding!
+# Gering-ding-ding-ding-dingeringeding!
+# Gering-ding-ding-ding-dingeringeding!"
