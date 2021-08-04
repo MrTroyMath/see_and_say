8aa7219ded8c625dbf760c279902f5a523025918
Abigail Faron <afaron@AFaronMBA.local>
2021-08-04 14:53:13 -0500

    actually saved those changes from merged

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-<<<<<<< HEAD
 images = ['grid_0.png', 'grid_1.png', 'grid_2.png', 'grid_3.png', 'grid_4.png',
     'grid_5.png', 'grid_6.png', 'grid_7.png', 'grid_8.png']
 
@@ -46,50 +45,6 @@ def show_grid():
     page_title = "Move Around A Grid"
     return render_template('grid.html', tab_title=tab_title, page_title=page_title,
         choices=choices, image=image, steps=steps)
-=======
-
-# images = ['grid_0.png', 'grid_1.png', 'grid_2.png', 'grid_3.png', 'grid_4.png',
-#     'grid_5.png', 'grid_6.png', 'grid_7.png', 'grid_8.png']
-
-images = ['dog.gif', 'cat.gif', 'bird.gif', 'mouse.gif', 'cow.gif',
-    'frog.gif', 'elephant.gif', 'duck.gif', 'fox.gif']
-    
-
-@app.route('/', methods=['GET', 'POST'])
-
-def show_grid():
-    noises = {
-    "dog":"Dog goes 'woof'",
-    "cat":"Cat goes 'meow'",
-    "bird":"Bird goes 'tweet'",
-    "mouse":"Mouse goes 'squeak'",
-    "cow":"cow goes 'moo'",
-    "frog":"Frog goes 'croak'",
-    "elephant":"the elephant goes 'toot'",
-    "duck":"ducks say 'quack'",
-    "fox":"What Does the fox say?"}
-    display_noise = 'Click on an animal to find out what it says'
-
-    if request.method == 'POST':
-        
-        name_value = request.form['animal_selection']
-        
-        print(name_value)
-
-        display_noise = noises[name_value]
-
-
-        # choice = int(request.form['choice'])    # Collect the radio button value from the form.
-        # old_steps = request.form['steps']       # Retrieve the old 'steps' string from the webpage. 
-        # steps = f"{old_steps}-{choice}"     # Generate a new 'steps' string.
-        # image = images[choice]              # Assign a file name from the images list.
-        # choices = fill_choices(choice)      # Call the fill_choices() function, which returns a dictionary of the allowed directions and values for the new box.
-    
-    
-    animals_list = list(noises.keys())
-      
-    return render_template('index.html', noises = noises , animals_list=animals_list, images = images, display_noise = display_noise)
->>>>>>> abby-branch
 
 if __name__ == '__main__':
     app.run()
@@ -107,8 +62,4 @@ if __name__ == '__main__':
 # What does the fox say?
 # "Ring-ding-ding-ding-dingeringeding!
 # Gering-ding-ding-ding-dingeringeding!
-<<<<<<< HEAD
 # Gering-ding-ding-ding-dingeringeding!"
-=======
-# Gering-ding-ding-ding-dingeringeding!"
->>>>>>> abby-branch
