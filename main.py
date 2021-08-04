53f47e2de88f1153e3620457838e6aa374cfc22a
Abigail Faron <afaron@AFaronMBA.local>
2021-08-04 15:27:21 -0500

    trying to make things pretty

--- main.py
+++ main.py
@@ -4,14 +4,9 @@ import random
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-# images = ['grid_0.png', 'grid_1.png', 'grid_2.png', 'grid_3.png', 'grid_4.png',
-#     'grid_5.png', 'grid_6.png', 'grid_7.png', 'grid_8.png']
-
 images = ['dog.gif', 'cat.gif', 'bird.gif', 'mouse.gif', 'cow.gif',
     'frog.gif', 'elephant.gif', 'duck.gif', 'fox.gif']
     
-
 @app.route('/', methods=['GET', 'POST'])
 
 def show_grid():
@@ -25,24 +20,19 @@ def show_grid():
     "elephant":"the elephant goes 'toot'",
     "duck":"ducks say 'quack'",
     "fox":"What Does the fox say?"}
+    
+    # Creates a default display noise 
     display_noise = 'Click on an animal to find out what it says'
 
     if request.method == 'POST':
         
         name_value = request.form['animal_selection']
         
-        print(name_value)
+        # print(name_value) #would display to terminal
 
         display_noise = noises[name_value]
-
-
-        # choice = int(request.form['choice'])    # Collect the radio button value from the form.
-        # old_steps = request.form['steps']       # Retrieve the old 'steps' string from the webpage. 
-        # steps = f"{old_steps}-{choice}"     # Generate a new 'steps' string.
-        # image = images[choice]              # Assign a file name from the images list.
-        # choices = fill_choices(choice)      # Call the fill_choices() function, which returns a dictionary of the allowed directions and values for the new box.
     
-    
+
     animals_list = list(noises.keys())
       
     return render_template('index.html', noises = noises , animals_list=animals_list, images = images, display_noise = display_noise)
